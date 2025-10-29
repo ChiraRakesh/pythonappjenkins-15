7384c5d08adf36385703e33575159a51ef4d302b
ChiraRakesh <chirarakesh1521@gmail.com>
2025-10-30 02:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-    return "Hello from Flask App! This is a Rakesh Chira` version."
+    return "Hello from python Flask App! This is a Rakesh Chira` python Flask ."
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
